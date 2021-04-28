fbd3dcc6796c316190dbeda56567337c16abd2e4
FrontendTerminator <mikutishvili.koba@gmail.com>
2021-04-28 21:05:09 +0300

    Updates

---
+++ precache-manifest.1e1d462c8b0a5a42458e03e086f535c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cd34d135c71666571052ebcba9322e",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d1cde6949962726e25ab",
+    "url": "/todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "ba64aac454efb58c17dd",
+    "url": "/todolist/static/js/2.5c048171.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.5c048171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cde6949962726e25ab",
+    "url": "/todolist/static/js/main.bab0384f.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
