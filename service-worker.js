236d50146054379a5328afa33b6d73e7793b9ebb
Koba Mikutishvili <k.mikutishvili@intelliventory.ai>
2023-04-27 16:58:41 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todolist/precache-manifest.efeb3117554e96911721eae5407ef1aa.js"
+  "/todolist/precache-manifest.34e4f54f54697ffac2a2abfe9e56b8f5.js"
 );
 
 self.addEventListener('message', (event) => {
