cba7d3c224e41ecc8bcfed43fda960636558a2bb
Koba Mikutishvili <k.mikutishvili@intelliventory.ai>
2025-02-18 20:23:33 +0300

    Updates

---
+++ precache-manifest.89738d6185137b4e462a89d3f6caea48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3ba56886ad901d72c8df3b395c22e4",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "51e6959babceb60096ab",
+    "url": "/Todolist/static/css/main.d64fd761.chunk.css"
+  },
+  {
+    "revision": "b64f6fd210e3ae110368",
+    "url": "/Todolist/static/js/2.3a6dc41a.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Todolist/static/js/2.3a6dc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e6959babceb60096ab",
+    "url": "/Todolist/static/js/main.908202ef.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
